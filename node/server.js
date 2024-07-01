502a4cf87360d2b709953d982d0d1de0774ee04d
Kenji Her <kenjito7@gmail.com>
2024-07-01 15:32:59 -0400

    added get for planets

--- node/server.js
+++ node/server.js
@@ -13,3 +13,19 @@ app.use(cors()); // Enable CORS for all routes
 app.use(express.json()); // Middleware to parse JSON bodies
 const PORT = 3000;
 
+app.get('/planets', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const planets = await collection.find({}).toArray();
+        res.json(planets);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Hmmm, something smells... No planets for you! ☹");
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
