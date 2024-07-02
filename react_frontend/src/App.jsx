340ef0282e8950e3148a2f6ce6928783fee08208
Xander Johnson <xanjay1671@gmail.com>
2024-07-02 13:53:43 -0400

    React routing troubles

--- react_frontend/src/App.jsx
+++ react_frontend/src/App.jsx
@@ -1,15 +1,28 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import CharacterList from './components/CharacterList'
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {}, []);
 
   return (
     <>
-      <CharacterList />
+      <Router>
+        <h1>SWAPI</h1>
+        {/* <Routes>
+                  <Route exact path="/" element={<Home data={data} handleDelete={handleDelete} page={page} setPage={setPage} />} />
+                  <Route path="/about" element={<About />} />
+                  <Route path="/add" element={ */}
+        <Routes>
+          <Route
+            exact path="/"
+            render={routeProps => <CharacterList {...props} {...routeProps} />}
+          />
+          {/* <Route path="/films" element={<FilmsList />} />
+        <Route path="/planets" element={<PlanetList/>} />
+        <Route path="/planets/*" element={<Planet data={this.props.params.splat} />} /> */}
+        </Routes>
+      </Router>
     </>
   )
 }
--- react_frontend/src/components/CharacterList.jsx
+++ react_frontend/src/components/CharacterList.jsx
@@ -1,31 +1,37 @@
 import React from "react";
 import { useEffect, useState } from "react";
 
-const CharacterList = () => {
-    const [data, setData] = useEffect([]);
+const CharacterList = (props) => {
+    const [data, setData] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
-          try {
-            const response = await fetch(`${import.meta.env.VITE_CHARACTERS_API_URL}`);
-            if (!response.ok) {
-              throw new Error('Data could not be fetched!');
+            try {
+                const response = await fetch(`${import.meta.env.VITE_CHARACTERS_API_URL}`);
+                if (!response.ok) {
+                    throw new Error('Data could not be fetched!');
+                }
+                const json_response = await response.json();
+                setData(json_response);
+            } catch (error) {
+                console.error('Error fetching characters:', error);
             }
-            const json_response = await response.json();
-            setData(json_response);
-          } catch (error) {
-            console.error('Error fetching socks:', error);
-          }
         };
-    
+
         fetchData();
-      }, [page]);
+    });
 
     return (
         <>
-            {data.map((character) => (
-                    <div></div>
-                ))}
+            <section id="charactersList">
+                {
+                    data.map((character) => (
+                        <div key={character.id} onClick={() => this.props.history.push(`/characters/${character.id}`)}>
+                            {character.name}
+                        </div>
+                    ))
+                }
+            </section >
         </>
     )
 };
